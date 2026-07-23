b83e6fc002907cc580066fbb1133646bb6699ebd
unknown <maksimssandykdorz4@gmail.com>
2026-07-23 14:41:10 +0800

    'sdfsdf'

---
+++ netlify/functions/request.ts
@@ -0,0 +1,23 @@
+import { prisma } from "../../lib/prisma";
+async function main() {
+  // Create a new user with a post
+  await prisma.user.create({
+    data: {
+      name: "Alice",
+      email: "alice@prisma.io",
+      password: 'fsdfsdfsd'
+      },
+    },
+)
+  // Fetch all users with their posts
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
+}
